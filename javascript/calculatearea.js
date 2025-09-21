8bfa2b3099f316ccad77d6c810d2dc3256c8e4ed
mirthas1 <nm.mirthas1@gmail.com>
2025-09-21 18:52:20 +0530

    if,loops

---
+++ javascript/calculatearea.js
@@ -0,0 +1,10 @@
+function area(a,b)
+{ var z=a*b
+    console.log("The area is : "+z)}
+area(10,20) 
+
+
+function add (x,y)
+{return (x+y)}
+var total=add(10,20)
+console.log(total)
--- javascript/fst.js
+++ javascript/fst.js
@@ -1,3 +1,11 @@
 let x=5;
 let y=6;
-console.log; x+y;
+console.log (x+y)
+function myname()
+{console.log("Monica")}
+myname()
+var a=10 
+var b=15
+function addition1()
+    {console.log(a+b)}
+addition1()
