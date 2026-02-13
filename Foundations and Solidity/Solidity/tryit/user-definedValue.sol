3b933f6b8e79da357a390d3814a45f10b64159f8
mirthas1 <nm.mirthas1@gmail.com>
2026-02-13 14:03:09 +0530

    user-defined values

---
+++ Foundations and Solidity/Solidity/tryit/user-definedValue.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.30;
+
+type UFixed256x18 is  uint256;
+
+library FixedMath  {
+
+    uint constant multiplier = 10**18;
+
+
+    function add(UFixed256x18 a, UFixed256x18 b) internal pure returns (UFixed256x18) {
+        return UFixed256x18.wrap(UFixed256x18.unwrap(a) + UFixed256x18.unwrap(b));
+    }
+
+    function mul(UFixed256x18 a, uint256 b) internal pure returns (UFixed256x18) {
+        return UFixed256x18.wrap(UFixed256x18.unwrap(a) * b);
+    }
+
+    function floor(UFixed256x18 a) internal pure returns (uint256) {
+        return UFixed256x18.unwrap(a) / multiplier;
+    }
+
+    function toUfixed(uint256 a) internal pure returns (UFixed256x18) {
+        return UFixed256x18.wrap(a * multiplier);
+    }
+}
