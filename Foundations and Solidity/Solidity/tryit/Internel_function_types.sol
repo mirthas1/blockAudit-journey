ea98bd4f248b5e79eb29c1d0c8916cbce0906fa4
mirthas1 <nm.mirthas1@gmail.com>
2026-02-19 13:22:34 +0530

    use internal function types

---
+++ Foundations and Solidity/Solidity/tryit/Internel_function_types.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.30;
+
+
+library ArrayUtils {
+    function map (uint[] memory self, function (uint) pure returns (uint) f)
+    internal
+    pure
+    returns (uint[] memory r)
+    {
+        r = new uint[](self.length);
+        for (uint i = 0; i < self.length; i++) {
+            r[i] = f(self[i]);
+        }
+    }
+
+    function reduce(uint[] memory self, function (uint,uint) pure returns(uint) f)
+    internal
+    pure
+    returns (uint r)
+    {
+        r = self[0];
+        for (uint i = 1; i < self.length; i++) {
+            r = f(r, self[i]);
+        }
+    }
+
+    function range(uint length) internal pure returns(uint[] memory r) {
+        r = new uint[](length);
+        for (uint i = 0; i < r.length; i++){
+            r[i] = i;
+        }
+    }
+} 
