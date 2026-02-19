5bd1be1daeb96d0a3c3e373bfad5340ef3b2f5bc
mirthas1 <nm.mirthas1@gmail.com>
2026-02-19 13:28:03 +0530

    use internal function types

--- Foundations and Solidity/Solidity/tryit/Internel_function_types.sol
+++ Foundations and Solidity/Solidity/tryit/Internel_function_types.sol
@@ -32,3 +32,20 @@ library ArrayUtils {
         }
     }
 } 
+
+contract pyramid {
+    using ArrayUtils for *;
+
+    function pyramid1(uint l) public pure returns(uint) {
+        return ArrayUtils.range(1).map(square).reduce(sum);
+    }
+
+    function square(uint x) internal pure returns(uint ){
+        return x *x;
+
+    }
+
+    function sum(uint x, uint y) internal pure returns (uint) {
+        return x + y;
+    }
+}
